5407d14e44e730293387d28c3517931e9b14d69f
Yana <yana.grin.2019@mail.ru>
2021-04-19 18:02:41 +0300

    111

---
+++ main1.py
@@ -0,0 +1,25 @@
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import random
+import os
+
+TOKEN = os.environ.get("VK_TOKEN", None)
+
+
+def main():
+    vk_session = vk_api.VkApi(token=TOKEN)
+    longpoll = VkBotLongPoll(vk_session, "204074800")
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            print(event)
+            print('Новое сообщение:')
+            print('Для меня от:', event.obj['from_id'])
+            print('Текст:', event.obj['text'])
+            vk = vk_session.get_api()
+            vk.messages.send(user_id=event.obj['from_id'],
+                             message="Спасибо, что написали нам. Мы обязательно ответим",
+                             random_id=random.randint(0, 2 ** 64))
+
+
+if __name__ == '__main__':
+    main()
