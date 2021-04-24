d0f52e81c8030df7a5cedaea7b5c6f1052de538b
Yana <yana.grin.2019@mail.ru>
2021-04-24 19:12:40 +0300

    try to add photo

--- main1.py
+++ main1.py
@@ -3,13 +3,10 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 import os
 
-from vk_api import VkApi
-from vk_api.upload import VkUpload
-from vk_api.utils import get_random_id
-
 TOKEN = os.environ.get("VK_TOKEN", None)
-PEER_ID = '204074800'
-def main1():
+
+
+def main():
     vk_session = vk_api.VkApi(token=TOKEN)
     longpoll = VkBotLongPoll(vk_session, "204074800")
     for event in longpoll.listen():
@@ -22,27 +19,7 @@ def main1():
             vk.messages.send(user_id=event.obj['from_id'],
                              message="Спасибо, что написали нам. Мы скоро ответим ))" + "\nНовое сообщение",
                              random_id=random.randint(0, 2 ** 64))
-    vk = vk_session.get_api()
-    upload = VkUpload(vk)
-    send_photo(vk, PEER_ID, *upload_photo(upload, 'im1.jpg'))
 
 
-def upload_photo(upload, photo):
-    response = upload.photo_messages(photo)[0]
-
-    owner_id = response['204074800']
-    photo_id = response['https://vplate.ru/images/article/orig/2019/03/kak-opredelit-pol-morskoj-svinki.jpg']
-    access_key = response['access_key']
-
-    return owner_id, photo_id, access_key
-
-def send_photo(vk, peer_id, owner_id, photo_id, access_key):
-    attachment = f'photo{owner_id}_{photo_id}_{access_key}'  #attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-    vk.messages.send(
-        random_id=get_random_id(),
-        peer_id=peer_id,
-        attachment=attachment
-    )
-
-if __name__ == '__main1__':
-    main1()
+if __name__ == '__main__':
+    main()
