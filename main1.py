d8d2dfd88b3cb2fc85c4eb13c6e60f1b003dbb12
Yana <yana.grin.2019@mail.ru>
2021-04-25 23:34:19 +0300

    соединение с хироку

--- main1.py
+++ main1.py
@@ -3,9 +3,8 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 import os
 
-#TOKEN = os.environ.get("VK_TOKEN", None)
-TOKEN = "65ea7c5f7de818f2874b1df87a3d68fb1901edf612ded3d4b3b2e2efab35ac0312ab54d4029758d6ff05e"
-
+# TOKEN = "65ea7c5f7de818f2874b1df87a3d68fb1901edf612ded3d4b3b2e2efab35ac0312ab54d4029758d6ff05e"
+TOKEN = os.environ.get("VK_TOKEN", None)
 steps = {
     0:{
         "msg": "Привет, мы хожем подобрать модель телевизора по вашим пожеланиям. Для этого напишите ДА"
